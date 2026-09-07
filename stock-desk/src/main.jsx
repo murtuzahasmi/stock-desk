079b170b65b4a7e252af5d55a4c2c42de597bb2a
murtuzahasmi <124737400+murtuzahasmi@users.noreply.github.com>
2026-09-07 12:15:12 +0300

    Add files via upload

--- stock-desk/src/main.jsx
+++ stock-desk/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import StockResearchDesk from "./StockResearchDesk.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
